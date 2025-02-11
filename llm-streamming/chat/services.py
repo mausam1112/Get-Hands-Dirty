b1a0cd81c930cfe811a444e723ba51b185099591
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-11 22:23:09 +0545

    implementing generate method to generate reponse from chat LLM model

--- llm-streamming/chat/services.py
+++ llm-streamming/chat/services.py
@@ -1,4 +1,6 @@
 from chat import utils
+from langchain_core.messages import SystemMessage, HumanMessage
+from fastapi import HTTPException
 
 
 class ChatService:
@@ -11,3 +13,19 @@ class ChatService:
         self.llm_name = llm_name
 
         self.llm = utils.load_llm(self.llm_type, self.llm_name)
+
+    async def generate(self, user_query: str):
+        """Method which generates the answer."""
+
+        if not user_query:
+            raise HTTPException(400, {"msg": "Please provide your query."})
+
+        messages = [
+            SystemMessage(
+                content="You are expert in the field of AI. Response with user query"
+            ),
+            HumanMessage(content=user_query),
+        ]
+
+        async for event in self.llm.astream(input=messages):
+            print(event.content, end=" ")
