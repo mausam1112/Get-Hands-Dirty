938d0a1b11892e0b268782afc6107dea86d947f4
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-14 22:54:40 +0545

    adding route for rendering chat ui html file

--- llm-streamming/main.py
+++ llm-streamming/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from chat.routes import router as chat_router
 
 
@@ -9,6 +10,11 @@ app = FastAPI(title="Streamming APP")
 app.include_router(chat_router)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def home():
+    return FileResponse("static/streaming_consumer.html")
 
 
 if __name__ == "__main__":
