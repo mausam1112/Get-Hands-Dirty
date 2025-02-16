69d9edbe0b9f88c8fbb928e8254d016637ccadb7
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-16 12:59:00 +0545

    self-attention impl v1

---
+++ llm-from-scratch/attention/self-attention.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.d_out = d_out
+        self.w_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.w_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.w_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        keys = x @ self.w_key
+        queries = x @ self.w_query
+        values = x @ self.w_value
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+if __name__ == "__main__":
+    inputs = torch.tensor(
+        [
+            [0.43, 0.15, 0.89],  # Your (x^1)
+            [0.55, 0.87, 0.66],  # journey (x^2)
+            [0.57, 0.85, 0.64],  # starts (x^3)
+            [0.22, 0.58, 0.33],  # with (x^4)
+            [0.77, 0.25, 0.10],  # one (x^5)
+            [0.05, 0.80, 0.55],  # step (x^6)
+        ]
+    )
+
+    torch.manual_seed(123)
+
+    d_in = inputs.shape[1]
+    d_out = 2
+
+    sa_v1 = SelfAttention_v1(d_in, d_out)
+    print(sa_v1(inputs))
