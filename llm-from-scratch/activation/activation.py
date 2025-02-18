c8346c809113f9011a4ab9ef7c41344bdedf628d
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-18 23:13:05 +0545

    implementing GELU activation function

---
+++ llm-from-scratch/activation/activation.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        # Approximation of standard Gaussian distribution
+        # GELU(x) ≈ 0.5 ⋅ x ⋅ (1 + tanh[√((2/π)) ⋅ (x + 0.044715 ⋅ x^3])
+        return (
+            0.5
+            * x
+            * (
+                1
+                + torch.tanh(
+                    torch.sqrt(torch.tensor(2.0 / torch.pi))
+                    * (x + 0.044715 * torch.pow(x, 3))
+                )
+            )
+        )
