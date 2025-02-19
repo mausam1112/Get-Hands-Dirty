f0205d95baa260ff20f9a4f65dce424cc2b64103
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-19 22:53:15 +0545

    tranformer block

---
+++ llm-from-scratch/transformer/transformer.py
@@ -0,0 +1,37 @@
+import torch
+from torch import nn
+from attention.self_attention import MultiHeadAttention
+from modules.modules import FeedForward
+from normalization.normalization import LayerNorm
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"],
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_resid = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_resid(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_resid(x)
+        x = x + shortcut
+
+        return x
