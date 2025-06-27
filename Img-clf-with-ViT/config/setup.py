93285d0960e61cd015f9fb4c81ce7bcddc0ddeea
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-06-28 00:56:27 +0545

    dataset path updated and hyperparameters added

--- Img-clf-with-ViT/config/setup.py
+++ Img-clf-with-ViT/config/setup.py
@@ -2,6 +2,28 @@ import os
 
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
-DATA_PATH = "D:/Projects/Hands-Dirty/data/clf/cat-dog dataset"
+DATA_PATH = "D:/Projects/Hands-Dirty/dataset/clf/cat-dog dataset"
 IMAGE_SIZE = (180, 180)
 BATCH_SIZE = 128
+
+learning_rate = 0.001
+weight_decay = 0.0001
+
+num_epochs = 10  # For real training, use num_epochs=100. 10 is a test value
+image_size = 72  # We'll resize input images to this size
+patch_size = 6  # Size of the patches to be extract from the input images
+
+# num_patches = (image_size // patch_size) ** 2
+num_patches = (IMAGE_SIZE[0] // patch_size) ** 2
+
+projection_dim = 64
+num_heads = 4
+transformer_units = [
+    projection_dim * 2,
+    projection_dim,
+]  # Size of the transformer layers
+transformer_layers = 8
+mlp_head_units = [
+    2048,
+    1024,
+]  # Size of the dense layers of the final classifier
