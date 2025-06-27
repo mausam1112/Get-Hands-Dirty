e920d6af59fe02758e269d974ed6a64fd10c90b4
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-06-28 00:58:07 +0545

    added function to visualize patches

--- Img-clf-with-ViT/data/visualize.py
+++ Img-clf-with-ViT/data/visualize.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from keras import ops
+from nn.layers.patch import Patches
+from config import setup
 
 
 def display_samples(ds):
@@ -12,4 +15,35 @@ def display_samples(ds):
             plt.title(labels[i].numpy())
             plt.axis("off")
         break
+    plt.show()
+
+
+def display_patches(ds):
+    data = next(ds.as_numpy_iterator())
+    images, target = data  # tuple of images (b, h, w, c) and target
+    # print(images.shape, target.shape)
+    image = images[np.random.choice(np.arange(images.shape[0]))]
+
+    plt.figure(figsize=(4, 4))
+    plt.imshow(image.astype("uint8"))
+    plt.axis("off")
+    plt.title("Sample image")
+    plt.show()
+
+    image_tensor = ops.convert_to_tensor([image])
+    patches = Patches(setup.patch_size)(image_tensor)
+
+    print(f"Image size: {setup.IMAGE_SIZE[0]} X {setup.IMAGE_SIZE[1]}")
+    print(f"Patch size: {setup.patch_size} X {setup.patch_size}")
+    print(f"Patches per image: {patches.shape[1]}")
+    print(f"Elements per patch: {patches.shape[-1]}")
+    print(f"{patches.shape=}")
+
+    n = int(np.sqrt(patches.shape[1]))
+    plt.figure(figsize=(4, 4))
+    for i, patch in enumerate(patches[0]):
+        ax = plt.subplot(n, n, i + 1)
+        patch_img = ops.reshape(patch, (setup.patch_size, setup.patch_size, 3))
+        plt.imshow(ops.convert_to_numpy(patch_img).astype("uint8"))
+        plt.axis("off")
     plt.show()
--- Img-clf-with-ViT/main.py
+++ Img-clf-with-ViT/main.py
@@ -1,10 +1,20 @@
 from data import load, visualize
+from nn.layers import patch
+from config import setup
 
 
 def main():
     load.filter_corrupted_images()
     train_ds, val_ds, test_ds = load.gen_train_val_data()
     visualize.display_samples(train_ds)
+    # list(train_ds.as_numpy_iterator())    # convert EagerTensor into numpy array
+
+    for idx, ele in enumerate(train_ds.as_numpy_iterator()):
+        # print(len(ele), 'image->', ele[0].shape, 'label->', ele[1].shape)
+        lyr_patch = patch.Patches(setup.patch_size)(ele[0])
+        break
+
+    visualize.display_patches(train_ds)
 
 
 if __name__ == "__main__":
