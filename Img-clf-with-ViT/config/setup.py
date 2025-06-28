de01ac8de01e778e042b2cc56bd42230e80acf1d
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-06-28 20:15:44 +0545

    added channel of image

--- Img-clf-with-ViT/config/setup.py
+++ Img-clf-with-ViT/config/setup.py
@@ -4,6 +4,7 @@ os.environ["KERAS_BACKEND"] = "tensorflow"
 
 DATA_PATH = "D:/Projects/Hands-Dirty/dataset/clf/cat-dog dataset"
 IMAGE_SIZE = (180, 180)
+CHANNEL = 3
 BATCH_SIZE = 128
 
 learning_rate = 0.001
