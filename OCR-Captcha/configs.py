8c60aa91cf67ebe938d123c1af0efda87572add3
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2026-02-03 22:47:52 +0000

    hyperparameters added and Paths Enum changed to String Enum

--- OCR-Captcha/configs.py
+++ OCR-Captcha/configs.py
@@ -1,6 +1,12 @@
-from enum import Enum
-from pathlib import Path
+from enum import Enum, StrEnum
+
+class Hyperparater(Enum):
+    BATCH_SIZE = 16     # Batch size for training and validation
+
+    # Desired image dimensions
+    IMG_WIDTH = 200
+    IMG_HEIGHT = 50
 
 
-class Paths(Enum):
-    DATA_DIR = Path("../dataset/ocr-captcha/captcha_images_v2")
+class Paths(StrEnum):
+    DATA_DIR = "../dataset/ocr-captcha/captcha_images_v2"
