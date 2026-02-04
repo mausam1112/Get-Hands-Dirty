65d580fcc55af93360498dc7c4183a1a3d974295
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2026-02-04 22:37:22 +0000

    added function to split entire dataset into train and test split

--- OCR-Captcha/preprocessing.py
+++ OCR-Captcha/preprocessing.py
@@ -1,4 +1,5 @@
-from keras import layers
+import keras
+from keras import layers, ops
 
 
 def character_mapping(characters):
@@ -12,3 +13,22 @@ def character_mapping(characters):
     )
 
     return char_to_num, num_to_char
+
+
+def split_data(images, labels, train_split=0.9, shuffle=True):
+    # calculate size of dataset
+    size = len(images)
+    # create array of indices and shuffle if required
+    indices = ops.arange(size)
+    if shuffle:
+        indices = keras.random.shuffle(indices)
+    
+    # calculate size of train set
+    train_size = int(size * train_split)
+
+    # split
+    x_train, y_train = images[indices[:train_size]], labels[indices[:train_size]]
+    x_valid, y_valid = images[indices[train_size:]], labels[indices[train_size:]]
+
+    return x_train, x_valid, y_train, y_valid
+
